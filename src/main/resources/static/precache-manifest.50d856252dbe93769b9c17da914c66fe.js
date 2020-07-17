c7df8753bc675451c62cfdbdc3a144da370b7b0b
kanchitbajaj8070 <31831699+kanchitbajaj8070@users.noreply.github.com>
2020-07-17 08:51:18 +0530

    final

---
+++ src/main/resources/static/precache-manifest.50d856252dbe93769b9c17da914c66fe.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6d7385e7600bb612c61ce5571a32606",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fbb66d653412d28bc66",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "30eea0697210a3f392ea",
+    "url": "/static/css/main.c00da318.chunk.css"
+  },
+  {
+    "revision": "8fbb66d653412d28bc66",
+    "url": "/static/js/2.b7678b7c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b7678b7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30eea0697210a3f392ea",
+    "url": "/static/js/main.7ad5c7e1.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  },
+  {
+    "revision": "8f6a99c3e35b20979aca4ba630b4c5be",
+    "url": "/static/media/login-background.8f6a99c3.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1560adba46ede4088fae06496f3674ba",
+    "url": "/static/media/ppm.1560adba.jpg"
+  }
+]);
