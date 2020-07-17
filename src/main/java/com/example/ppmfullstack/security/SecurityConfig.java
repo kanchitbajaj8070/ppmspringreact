02eea9d1d5f7647a401aa35eafe88b1c2577b76a
kanchitbajaj8070 <31831699+kanchitbajaj8070@users.noreply.github.com>
2020-07-17 10:13:07 +0530

    final

--- src/main/java/com/example/ppmfullstack/security/SecurityConfig.java
+++ src/main/java/com/example/ppmfullstack/security/SecurityConfig.java
@@ -75,18 +75,18 @@ private CustomUserDetailsService customUserDetailsService;
     {
         return new JwtAuthenticationFilter();
     }
-    @Bean
+    /*@Bean
     public FilterRegistrationBean simpleCorsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         // *** URL below needs to match the Vue client URL and port ***
-        config.setAllowedOrigins(Collections.singletonList("https://productivityappkanban.herokuapp.com"));
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
         config.setAllowedMethods(Collections.singletonList("*"));
         config.setAllowedHeaders(Collections.singletonList("*"));
         source.registerCorsConfiguration("/**", config);
         FilterRegistrationBean bean = new FilterRegistrationBean<>(new CorsFilter(source));
         bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
         return bean;
-    }
+    }*/
 }
